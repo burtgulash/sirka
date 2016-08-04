f842738c17e8320e3b222177cdaa22ce06b79824
TM <none@none>
2016-08-04 16:18:43 +0200

    Pass merge without dups test

--- src/postings/cursor/merge/with_duplicates.rs
+++ src/postings/cursor/merge/with_duplicates.rs
@@ -43,9 +43,8 @@ impl<C: PostingsCursor> Merge<C> {
     pub fn new(to_merge: Vec<C>) -> Self {
         let size = to_merge.iter().map(|c| c.remains()).fold(0, |acc, x| acc + x);
         let mut heap = create_heap(to_merge);
-        let first = heap.pop();
         Merge{
-            current_ptr: first,
+            current_ptr: None,
             frontier: heap,
             size: size,
         }
@@ -57,30 +56,100 @@ impl<C: PostingsCursor> PostingsCursor for Merge<C> {
     type TS = C::TS;
     type PS = C::PS;
 
+
+    fn remains(&self) -> usize {
+        self.size
+    }
+
     unsafe fn current(&self) -> DocId {
         self.current_ptr.as_ref().unwrap().cursor.current()
     }
 
+    fn catch_up(&mut self, result: &mut VecPostings) -> usize {
+        self.current_ptr.as_mut().unwrap().cursor.catch_up(result)
+    }
+
     fn advance(&mut self) -> Option<DocId> {
+        if let Some(mut ptr) = self.current_ptr.take() {
+            if let Some(_) = ptr.cursor.advance() {
+                self.frontier.push(ptr);
+            }
+        } 
+        
+        self.current_ptr = self.frontier.pop();
+        if let Some(ref ptr) = self.current_ptr {
+            unsafe {Some(ptr.cursor.current())}
+        } else {
+            None
+        }
+        /*
+        if let Some(ptr) = self.frontier.pop() {
+            let doc_id = unsafe {ptr.cursor.current()};
+            self.current_ptr = Some(ptr);
+            return Some(doc_id);
+        } else {
+            return None;
+        };
+
+        let doc_id = unsafe {ptr.cursor.current()};
+        if let Some(doc_id) = ptr.cursor.advance() {
+            self.frontier.push(ptr);
+            self.current_ptr = self.frontier.pop();
+        }
+        Some(doc_id)
+
+        */
+            /*
         if let Some(mut ptr) = self.current_ptr.take() {
             if let Some(doc_id) = ptr.cursor.advance() {
                 self.frontier.push(ptr);
                 self.current_ptr = self.frontier.pop();
                 return Some(doc_id);
             }
+        } else if let Some(ptr) = self.frontier.pop() {
+            self.current_ptr = ptr;
+            let doc_id = unsafe {ptr.cursor.current()};
+            Some(doc_id)
         }
         None
+        */
     }
+}
 
-    fn catch_up(&mut self, result: &mut VecPostings) -> usize {
-        if let Some(ref mut ptr) = self.current_ptr {
-            ptr.cursor.catch_up(result)
-        } else {
-            0
-        }
-    }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use postings::{RawCursor,VecPostings,Postings,PostingsCursor,Sequence,SequenceStorage};
 
-    fn remains(&self) -> usize {
-        self.size
+    #[test]
+    fn test_merge_with_duplicates() {
+        let ps1 = VecPostings {
+            docs: vec![1, 2],
+            tfs: vec![0, 1, 2], // NOTE: tfs must already be cumulated!
+            positions: vec![1, 2],
+        };
+        let ps2 = VecPostings {
+            docs: vec![2, 3],
+            tfs: vec![0, 2, 5], // NOTE: tfs must already be cumulated!
+            positions: vec![1, 2, 1, 2, 3],
+        };
+
+        let ps1c = RawCursor::new(Postings {
+            docs: (&ps1.docs).to_sequence(),
+            tfs: (&ps1.tfs).to_sequence(),
+            positions: (&ps1.positions).to_sequence(),
+        });
+        let ps2c = RawCursor::new(Postings {
+            docs: (&ps2.docs).to_sequence(),
+            tfs: (&ps2.tfs).to_sequence(),
+            positions: (&ps2.positions).to_sequence(),
+        });
+
+        let mut merger = Merge::new(vec![ps1c, ps2c]);
+        let mut merged = merger.collect();
+
+        assert_eq!(merged.docs, vec![1, 2, 2, 3]);
+        assert_eq!(merged.tfs, vec![1, 2, 1, 3]);
+        assert_eq!(merged.positions, vec![1, 1, 2, 2, 1, 2, 3]);
     }
 }
